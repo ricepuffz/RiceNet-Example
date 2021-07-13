d10906a5c97251674390a17564b87204328f249a
ricepuffz <28723269+ricepuffz@users.noreply.github.com>
2021-07-14 01:36:27 +0200

    Add project files.

---
+++ Client Example/ClientExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using RiceNet;
+
+namespace Client_Example
+{
+    class ClientExample
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("localhost", 45123);
+            client.SendString("beep boop hi :)");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ RiceNet Example/ServerExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RiceNet;
+
+namespace RiceNet_Example
+{
+    class ServerExample
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server(45123);
+            AcceptedClient acceptedClient = null;
+
+            while (acceptedClient == null)
+            {
+                if (server.AcceptedClientsCount() > 0)
+                    acceptedClient = server.GetAcceptedClient(0);
+            }
+            
+            Console.WriteLine(acceptedClient.ReceiveString());
+
+            server.Stop();
+        }
+    }
+}
